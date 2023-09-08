1912f7b86167343b5ac90a5293f01de4e4206756
LorenzoPeracchio <124535474+LorenzoPeracchio@users.noreply.github.com>
2023-09-08 11:35:45 +0200

    Update lumache.py

--- lumache.py
+++ lumache.py
@@ -17,18 +17,18 @@ __version__ = "0.1.0"
 
 
 def create_autoencoder(layer_sizes):
+    """ Gets an autoencoder model with the specified sizes of the layers.
+
+    Parameters
+    ----------
+    layer_sizes: list
+        A list containing the number of nodes of each layer of the encoder (decoder built with symmetry).
+
+    Returns
+    -------
+        numpy.ndarray
+            An instance of the autoencoder model.
     """
-    Gets an autoencoder model with the specified sizes of the layers.
 
-    This function gets an autoencoder model using the `AE` class, implemented as a PyTorch module, with the specified
-    layers' sizes.
-    The autoencoder is used for the implementation of the Density Principle.
-
-    :param list layer_sizes: A list containing the number of nodes of each layer of the encoder (decoder built with
-     symmetry).
-
-    :return: An instance of the autoencoder model.
-    :rtype: AE
-    """
     ae = AE(layer_sizes)
     return ae
